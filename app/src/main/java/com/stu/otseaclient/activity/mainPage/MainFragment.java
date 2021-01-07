0b328935c1b649fd7a8d1dc5e1ce42d4294e6ed3
17fyang <1099640098@qq.com>
2021-01-07 22:22:16 +0800

    增加首页和发现页

--- app/src/main/java/com/stu/otseaclient/activity/mainPage/MainFragment.java
+++ app/src/main/java/com/stu/otseaclient/activity/mainPage/MainFragment.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import com.stu.com.R;
+import com.stu.otseaclient.component.image.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: 乌鸦坐飞机亠
@@ -13,10 +19,36 @@ import com.stu.com.R;
  * @Description:
  */
 public class MainFragment extends Fragment {
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        initBanner();
+
         return view;
+    }
+
+    /**
+     * 初始化banner轮播图
+     */
+    private void initBanner() {
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.banner_1);
+        images.add(R.drawable.banner_2);
+        images.add(R.drawable.banner_3);
+        images.add(R.drawable.banner_4);
+
+        Banner banner = view.findViewById(R.id.main_banner);
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //设置轮播时间
+        banner.setDelayTime(3500);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
     }
 }
